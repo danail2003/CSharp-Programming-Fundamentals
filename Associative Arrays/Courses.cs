2e1671634c71b2fa94fa9aefa2da51488615b889
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-03-01 23:45:46 +0200

    Update Courses.cs

--- Associative Arrays/Courses.cs
+++ Associative Arrays/Courses.cs
@@ -24,11 +24,11 @@ public class Program
             command = Console.ReadLine();
         }
 
-        foreach (var course in allStudents.OrderByDescending(x=>x.Value.Count))
+        foreach (var course in allStudents.OrderByDescending(x => x.Value.Count))
         {
             Console.WriteLine($"{course.Key}: {course.Value.Count}");
 
-            foreach (var student in course.Value.OrderBy(x=>x))
+            foreach (var student in course.Value.OrderBy(x => x))
             {
                 Console.WriteLine($"-- {string.Join(" ", student)}");
             }
