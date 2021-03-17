b8e507674e96be1384e14ada82487854f1ee2aa6
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-03-17 19:17:10 +0200

    Update and rename Match Dates to Regex/Match Dates.cs

--- Regex/Match Dates.cs
+++ Regex/Match Dates.cs
@@ -8,6 +8,7 @@ namespace Match_Dates
         static void Main()
         {
             string dates = Console.ReadLine();
+            
             Regex pattern = new Regex(@"\b(?<day>\d{2})([.\/-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b");
             MatchCollection matches = pattern.Matches(dates);
 
