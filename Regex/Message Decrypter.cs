db627b6db5901b600f3f0b32d63c6c2a430221e7
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-03-19 18:54:22 +0200

    Update and rename Message Decrypter to Regex/Message Decrypter.cs

--- Regex/Message Decrypter.cs
+++ Regex/Message Decrypter.cs
@@ -8,6 +8,7 @@ namespace Message_Decrypter
         static void Main()
         {
             int count = int.Parse(Console.ReadLine());
+            
             Regex regex = new Regex(@"^(\$|%)(?<word>[A-Z][a-z]{3,})\1: \[(?<first>\d*)]\|\[(?<second>\d*)]\|\[(?<third>\d*)]\|$");
 
             for (int i = 0; i < count; i++)
@@ -21,6 +22,7 @@ namespace Message_Decrypter
                     int firstSymbol = int.Parse(match.Groups["first"].Value);
                     int secondSymbol = int.Parse(match.Groups["second"].Value);
                     int thirdSymbol = int.Parse(match.Groups["third"].Value);
+                    
                     char first = (char)firstSymbol;
                     char second = (char)secondSymbol;
                     char third = (char)thirdSymbol;
