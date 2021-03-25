bd326d0d5c9da35154bb72e84f1427c53edf9d3a
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-03-25 19:17:58 +0200

    Update and rename Smallest of Three Numbers to Methods/Smallest of Three Numbers.cs

--- Methods/Smallest of Three Numbers.cs
+++ Methods/Smallest of Three Numbers.cs
@@ -9,20 +9,21 @@ namespace Smallest_of_Three_Numbers
             int firstNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
+            
             GetSmallestNumber(firstNumber, secondNumber, thirdNumber);
         }
 
         static void GetSmallestNumber(int firstNumber, int secondNumber, int thirdNumber)
         {
-            if(firstNumber <= secondNumber && firstNumber <= thirdNumber)
+            if (firstNumber <= secondNumber && firstNumber <= thirdNumber)
             {
                 Console.WriteLine(firstNumber);
             }
-            else if(secondNumber <= firstNumber && secondNumber <= thirdNumber)
+            else if (secondNumber <= firstNumber && secondNumber <= thirdNumber)
             {
                 Console.WriteLine(secondNumber);
             }
-            else if(thirdNumber <= firstNumber && thirdNumber <= secondNumber)
+            else if (thirdNumber <= firstNumber && thirdNumber <= secondNumber)
             {
                 Console.WriteLine(thirdNumber);
             }
