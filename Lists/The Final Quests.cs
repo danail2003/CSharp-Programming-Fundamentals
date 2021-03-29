dde0e69b2b7cc8063819fce25ecba929e7f1e616
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-03-29 20:33:04 +0300

    Update and rename The Final Quests to Lists/The Final Quests.cs

--- Lists/The Final Quests.cs
+++ Lists/The Final Quests.cs
@@ -19,14 +19,14 @@ namespace The_Final_Quest
                 {
                     int index = int.Parse(token[1]);
 
-                    if(index >= -1 && index < words.Count - 1)
+                    if (index >= -1 && index < words.Count - 1)
                     {
                         words.RemoveAt(index + 1);
                     }
                 }
                 else if (token[0] == "Swap")
                 {
-                    if(words.Contains(token[1]) && words.Contains(token[2]))
+                    if (words.Contains(token[1]) && words.Contains(token[2]))
                     {
                         for (int i = 0; i < words.Count; i++)
                         {
@@ -34,7 +34,7 @@ namespace The_Final_Quest
                             {
                                 words[i] = token[2];
                             }
-                            else if(words[i] == token[2])
+                            else if (words[i] == token[2])
                             {
                                 words[i] = token[1];
                             }
@@ -45,7 +45,7 @@ namespace The_Final_Quest
                 {
                     int index = int.Parse(token[2]);
 
-                    if(index > 0 && index <= words.Count)
+                    if (index > 0 && index <= words.Count)
                     {
                         for (int i = 0; i < words.Count; i++)
                         {
